2375d4cc0e31df22ee5ecd6646d051c50a50925b
Diya Salam <sdiya.salam.github@gmail.com>
2025-10-29 12:07:55 -0400

    adding important annotations

---
+++ src/main/java/com/advancedspring/annotations/PrePostContextLauncerApplication.java
@@ -0,0 +1,49 @@
+package com.advancedspring.annotations;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+
+@Component
+class SomeClass{
+	private SomeDependency someDependency;
+	
+	public SomeClass(SomeDependency someDependency) {
+		super();
+		this.someDependency = someDependency;
+		System.out.println("All the dependencies are ready");
+	}
+	@PostConstruct
+	public void initilalize() {
+		someDependency.getReady();
+	}
+	@PreDestroy
+	public void cleanUp() {
+		System.out.println("Cleaning up");
+	}
+}
+@Component
+class SomeDependency{
+
+	public void getReady() {
+		System.out.println("Using some logic using SomeDependency");
+		
+	}
+	
+}
+@Configuration
+@ComponentScan
+public class PrePostContextLauncerApplication {
+	public static void main(String[] args) {
+		try(var context = new AnnotationConfigApplicationContext(PrePostContextLauncerApplication.class)){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		}
+	}
+}
